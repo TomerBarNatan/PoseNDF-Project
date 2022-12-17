208bc61821e4cf1017c344bf6735ffd3a1437149
tomer.bar.natan2 <tomer.bar.natan2@gmail.com>
2022-12-17 15:19:16 +0200

    create main

---
+++ main.py
@@ -0,0 +1,31 @@
+import argparse
+import yaml
+from trainer import PoseNDF_trainer
+import shutil
+
+
+def train(config, config_path):
+    trainer = PoseNDF_trainer(config)
+    copy_config = f"{config['experiment']['root_dir']}/{trainer.exp_name}/config.yaml"
+    shutil.copyfile(config_path, copy_config)
+    val = config['experiment']['val']
+    test = config['experiment']['test']
+    if test:
+        trainer.inference(trainer.ep)
+    for i in range(trainer.ep, config['train']['max_epoch']):
+        _, _ = trainer.train_model(i)
+        if val and i % 100 == 0:
+            trainer.validate(i)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Train PoseNDF.'
+    )
+    parser.add_argument('--config', '-c', default='configs/amass.yaml', type=str, help='Path to config file.')
+    parser.add_argument('--test', '-t', action="store_true")
+    args = parser.parse_args()
+    config_path = args.config
+    with open(config_path, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    train(config, config_path)
